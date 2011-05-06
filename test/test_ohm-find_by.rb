ed7835f8b353e2a8d2b06517193b9a5d858cd730
Bruno Aguirre <yo@brunoaguirre.com>
2011-05-06 09:16:32 -0300

    Switched tests to cutest

--- test/test_ohm-find_by.rb
+++ test/test_ohm-find_by.rb
@@ -1,4 +1,9 @@
-require 'helper'
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'ohm'
+require 'ohm/find_by'
+
+Ohm.connect(:port => 6379)
 
 class Post < Ohm::Model
   attribute :name
@@ -9,28 +14,23 @@ class Post < Ohm::Model
   index :really_long_index_name
 end
 
-class TestOhmFindBy < Test::Unit::TestCase
-  context "searching within a Model" do
-    setup do
-      Ohm.flush
-      @created_post = Post.create(:name => "First Post!", :author => "elCuervo", :really_long_index_name => "yeah")
-    end
+setup do
+  Ohm.flush
+  Post.create(:name => "First Post!", :author => "elCuervo", :really_long_index_name => "yeah")
+end
 
-    should "find an instance with .find_by_{index_name}" do
-      searched_post = Post.find_by_name("First Post!").first
-      assert searched_post.id == @created_post.id
-    end
+test "find an instance with .find_by_{index_name}" do |created_post|
+  searched_post = Post.find_by_name("First Post!").first
+  assert searched_post.id == created_post.id
+end
 
-    should "find an instance even with a really long index name" do
-      searched_post = Post.find_by_really_long_index_name("yeah").first
-      assert searched_post.id == @created_post.id
-    end
+test "find an instance even with a really long index name" do |created_post|
+  searched_post = Post.find_by_really_long_index_name("yeah").first
+  assert searched_post.id == created_post.id
+end
 
-    should "not find an instance if the index does not exist" do
-      assert_raise Ohm::Model::IndexNotFound do
-        searched_post = Post.find_by_author("elCuervo")
-      end
-    end
-
+test "not find an instance if the index does not exist" do |created_post|
+  assert_raise Ohm::Model::IndexNotFound do
+    searched_post = Post.find_by_author("elCuervo")
   end
 end
